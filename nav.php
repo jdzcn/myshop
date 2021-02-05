5c96146e8749e9402b268609e3534b610af18978
root <root@sbhost.asia-east1-b.c.feisty-outrider-270809.internal>
2021-02-05 09:18:33 +0800

    update nav.php

--- nav.php
+++ nav.php
@@ -5,7 +5,14 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,minimum-scale=1.0,user-scalable=0" />
 <title>店小二</title>
 <link rel="stylesheet" media="screen" href="myshop.css"/>
-
+<style>
+	body {
+		box-sizing: border-box;
+		min-width:200px;
+		max-width:980px;
+		margin: 0 auto;
+	}
+</style>
 </head>
 <body>
 <nav>
